d2b03741162d01a6e4653dfc68231f82484c88a8
menace31 <54880433+menace31@users.noreply.github.com>
2022-10-12 12:09:50 +0200

    finition question 1

---
+++ hackathon.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+def cleanData(data_B,minDate,maxDate):
+    data_B = data_B[(data_B[" Q_FG"]==0)]
+    data_B = data_B[(data_B["    DATE"]>=minDate)]
+    data_B = data_B[(data_B["    DATE"]<=maxDate)]
+
+    return data_B
+
+
+# open and read data
+data_E = pd.read_csv("database/Elsenborn.csv", sep = ',')
+data_B = pd.read_csv("database/Beauvechain.csv", sep = ',')
+
+minDate = 20170101
+maxDate = 20210101
+
+# sort the database to remove wrong data
+data_B = cleanData(data_B,minDate,maxDate)
+
+testcleanData(data_B,minDate,maxDate)
